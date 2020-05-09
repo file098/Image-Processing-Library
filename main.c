0981ea969dd043ccdf3647aee363f3f9e63ac122
Filippo Di Gennaro <filippodigennaro@Filippos-MBP.homenet.telecomitalia.it>
2020-05-09 17:42:34 +0200

    fix

--- main.c
+++ main.c
@@ -5,8 +5,7 @@
 #include "ip_lib.h"
 
 int main(){
-
     ip_mat * mat1 = ip_mat_create(2,2,3,4);
-    ip_mat_show(ip_mat_subset(mat1,0,1,0,1));
+    ip_mat_show(ip_mat_subset(mat1,0,2,0,2));
     return 0;
 }
